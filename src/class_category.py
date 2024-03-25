d655daccf2fdcb2fc8167c5adad53c68ca4b49c3
AlexanderMalach <80158382+AlexanderMalach@users.noreply.github.com>
2024-03-25 17:32:56 +0300

    Update class_category.py

--- src/class_category.py
+++ src/class_category.py
@@ -6,23 +6,20 @@ class Category:
     def __init__(self, name: str, description: str, products: list):
         self.name = name
         self.description = description
-        self._products = []  # Приватный атрибут для хранения списка товаров
-        self.add_products(products)  # Используем метод для добавления товаров
+        self.__products = []  # Приватный атрибут для хранения списка товаров
+        self.add_products(products)  # Добавляем товары при инициализации
         Category.total_categories += 1
         Category.total_products += len(products)
 
     def add_products(self, products: list):
         """Метод для добавления товаров в категорию"""
         for product in products:
-            self._products.append(product)
+            self.__products.append(product)
 
-    def get_products(self):
-        """Метод для получения списка товаров"""
-        return self._products
-
-    def products_info(self):
-        """Геттер для вывода информации о товарах"""
-        for product in self._products:
-            print(f"{product}, 80 руб. Остаток: 15 шт.")
-
-
+    @property
+    def product_info(self):
+        """Метод для вывода информации о товарах"""
+        list_info = []
+        for product in self.__products:
+            list_info.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity_stock} шт.")
+        return list_info
