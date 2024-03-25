a4b4b8bb97bde2b686e8e4ccb8dcbd470226bb1e
79096 <a.malakhinsky@gmail.com>
2024-03-25 17:23:05 +0300

    create cods and tests 24032024

--- src/class_category.py
+++ src/class_category.py
@@ -7,18 +7,18 @@ class Category:
         self.name = name
         self.description = description
         self.__products = []  # Приватный атрибут для хранения списка товаров
+        self.add_products(products)  # Добавляем товары при инициализации
         Category.total_categories += 1
         Category.total_products += len(products)
 
-    def add_products(self, product: list):
+    def add_products(self, products: list):
         """Метод для добавления товаров в категорию"""
-        self.__products.append(product)
+        for product in products:
+            self.__products.append(product)
 
-    @property
-    def product_info(self, price: float, quantity_stock: int):
-        """Геттер для вывода информации о товарах"""
+    def product_info(self):
+        """Метод для вывода информации о товарах"""
         list_info = []
         for product in self.__products:
-            list_info.append(f"{product}, {price} руб. Остаток: {quantity_stock} шт.")
+            list_info.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity_stock} шт.")
         return list_info
-
--- tests/test_class_category.py
+++ tests/test_class_category.py
@@ -1,23 +1,23 @@
+import pytest
+
 from src.class_category import Category
+from src.class_product import Product
 
 
-def test_category_creation():
-    category = Category("Электроника", "Товары в категории электроники", ["Телефон", "Планшет"])
-    assert category.name == "Электроника"
-    assert category.description == "Товары в категории электроники"
+@pytest.fixture
+def sample_category():
+    products = [Product("Product 1", "Description 1", 10.0, 100), Product("Product 2", "Description 2", 20.0, 50)]
+    return Category("Test Category", "Test Description", products)
+
+
+def test_category_creation(sample_category):
+    assert sample_category.name == "Test Category"
+    assert sample_category.description == "Test Description"
+    assert len(sample_category._Category__products) == 2
     assert Category.total_categories == 1
     assert Category.total_products == 2
 
 
-def test_add_products():
-    category = Category("Электроника", "Товары в категории электроники", [])
-    category.add_products(["Телефон", "Планшет"])
-    assert len(category.__products) == 2
-
-def test_product_info():
-    category = Category("Электроника", "Товары в категории электроники", ["Телефон", "Планшет"])
-    product_info = category.product_info(1000, 10)
-    assert len(product_info) == 2
-    assert product_info[0] == "Телефон, 1000 руб. Остаток: 10 шт."
-    assert product_info[1] == "Планшет, 1000 руб. Остаток: 10 шт."
-
+def test_product_info(sample_category):
+    expected_info = ["Product 1, 10.0 руб. Остаток: 100 шт.", "Product 2, 20.0 руб. Остаток: 50 шт."]
+    assert sample_category.product_info() == expected_info
