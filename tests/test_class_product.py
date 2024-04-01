5ac3a30f7399bde2a9e49f6820780789bc970ff1
79096 <a.malakhinsky@gmail.com>
2024-04-01 14:17:00 +0300

    len

--- tests/test_class_product.py
+++ tests/test_class_product.py
@@ -8,18 +8,19 @@ def sample_product():
     return Product("Test Product", "Test Description", 10.0, 100)
 
 
-def test_product_creation(sample_product):
+def test_create_product(sample_product):
     assert sample_product.name == "Test Product"
     assert sample_product.description == "Test Description"
     assert sample_product.price == 10.0
     assert sample_product.quantity == 100
 
 
-def test_set_price(sample_product):
+def test_price_getter_and_setter(sample_product):
     sample_product.price = 20.0
     assert sample_product.price == 20.0
 
 
-def test_set_price_negative(sample_product):
-    sample_product.price = -10.0
-    assert sample_product.price == 10.0  # Проверяем, что цена осталась прежней
+def test_add_method(sample_product):
+    other_product = Product("Other Product", "Other Description", 15.0, 50)
+    total_price = sample_product + other_product
+    assert total_price == 1750.0
