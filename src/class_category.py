60ac3ccad93e03b9d52360ef9aa0f883389f96b4
79096 <a.malakhinsky@gmail.com>
2024-04-02 15:20:09 +0300

    anot.

--- src/class_category.py
+++ src/class_category.py
@@ -18,10 +18,10 @@ class Category:
         self.__products.append(product)
         Category.total_products += 1
 
-    def __len__(self) -> int():
+    def __len__(self) -> int:
         return len(self.__products)
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Метод для вывода информации о товарах"""
         total_product = 0
         for i in self.__products:
