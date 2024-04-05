256ecb7a06e92242945ae2ef3c82b71007570faa
79096 <a.malakhinsky@gmail.com>
2024-04-05 18:58:21 +0300

    dfg

--- tests/test_class_product.py
+++ tests/test_class_product.py
@@ -17,6 +17,7 @@ def smartphone_product():
 def grass_product():
     return Grass("Test Product2", "Test Description2", 20.0, 200, "Russian", 10,  "red")
 
+
 def test_create_product(sample_product):
     assert sample_product.name == "Test Product"
     assert sample_product.description == "Test Description"
@@ -57,3 +58,22 @@ def test_add_smartphone_and_product(smartphone_product):
     assert smartphone_product + other_product is TypeError
 
 
+def test_create_grass(grass_product):
+    assert grass_product.name == "Test Product2"
+    assert grass_product.description == "Test Description2"
+    assert grass_product.price == 20.0
+    assert grass_product.quantity == 200
+    assert grass_product.country == "Russian"
+    assert grass_product.germination_period == 10
+    assert grass_product.color == "red"
+
+
+def test_add_method_grass(grass_product):
+    other_product = Grass("Test Product2", "Test Description2", 20.0, 200, "Russian", 10,  "red")
+    total_price = grass_product + other_product
+    assert total_price == 8000.0
+
+
+def test_add_grass_and_product(grass_product):
+    other_product = Product("Other Product", "Other Description", 15.0, 50)
+    assert grass_product + other_product is TypeError
