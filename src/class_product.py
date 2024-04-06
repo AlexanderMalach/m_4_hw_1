acebafc8a766b9df7cd1b3971e969e0e263ed3b2
79096 <a.malakhinsky@gmail.com>
2024-04-06 21:18:34 +0300

    add abc and @abstractmethod

--- src/class_product.py
+++ src/class_product.py
@@ -5,7 +5,7 @@ class AbsProduct(ABC):
 
     @abstractmethod
     def __init__(self):
-        super().__init__()
+        # super().__init__()
         pass
 
     @abstractmethod
@@ -16,13 +16,13 @@ class AbsProduct(ABC):
 class CreationInfoMixin:
     """Миксин для вывода информации о создании объекта"""
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self):
+        super().__init__()
         print(repr(self))
 
     def __repr__(self):
-        list_p = [f"{key}: {value}" for key, value in self.__dict__.items()]
-        return f"Создан объект класса {self.__class__.__name__}: {', '.join(list_p)}"
+        return (f"Создан объект класса {self.__class__.__name__}: "
+                f"{', '.join([f"{key}: {value}" for key, value in self.__dict__.items()])}")
 
 
 class Product(AbsProduct, CreationInfoMixin):
@@ -33,12 +33,11 @@ class Product(AbsProduct, CreationInfoMixin):
     quantity: int
 
     def __init__(self, name: str, description: str, price: float, quantity: int):
-        super().__init__()
         self.name = name
         self.description = description
         self.__price = price
         self.quantity = quantity
-
+        CreationInfoMixin.__init__(self)
     @property
     def price(self):
         """Геттер для атрибута цены"""
@@ -110,3 +109,4 @@ class Grass(Product):
 product1 = Product('Продукт1', 'Описание продукта', 1200, 10)
 smartphone1 = Smartphone('Смартфон1', 'Описание смартфона', 1000, 5, 2.3, 'Модель1', 64, 'Черный')
 grass1 = Grass('Трава1', 'Описание травы', 500, 20, 'Страна1', 14, 'Зеленый')
+print(Product.__mro__)
